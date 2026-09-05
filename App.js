e0f084dc38d0058a5110cce440f55556000b6bfa
saurabhk21911-glitch <saurabhk21911@gmail.com>
2026-09-06 03:38:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
-import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 const { width, height } = Dimensions.get('window');
 const ADMIN_PASS = "DishuRajvanshi7740";
 const LOGO_URL = "https://i.postimg.cc/SRnbwx8q/file-000000007a98820887b7f176e5d5470c.png";
