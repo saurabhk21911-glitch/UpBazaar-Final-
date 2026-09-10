d55fd1679d7920d09a9c93241140845f93ae0a70
saurabhk21911-glitch <saurabhk21911@gmail.com>
2026-09-11 00:52:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -21,7 +21,8 @@ function getDistanceKm(lat1, lon1, lat2, lon2) {
   const dLon = (lon2-lon1) * Math.PI/180;
   const a = Math.sin(dLat/2)**2 + Math.cos(lat1*Math.PI/180)*Math.cos(lat2*Math.PI/180)*Math.sin(dLon/2)**2;
   return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-}const firebaseConfig = {
+ }
+  const firebaseConfig = {
   apiKey: "AIzaSyA9ouZ2VJJME5vTCTnrsMfYg0uWGru606I",
   authDomain: "upbazaar.firebaseapp.com",
   projectId: "upbazaar",
